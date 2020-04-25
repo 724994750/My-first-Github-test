4e1bc7446cf1b7481c4a3416a5f343ba90deb02e
724994750 <60214142+724994750@users.noreply.github.com>
2020-04-25 12:06:35 +0800

    上传一个文件
    第一次使用，上传文件

---
+++ 15.递归——雪花.py
@@ -0,0 +1,30 @@
+#KochDrawV1.py
+import turtle
+
+def koch(size,n):
+    if n==0:
+        turtle.fd(size)
+    else:
+        for angle in [0,60,-120,60]:
+            turtle.left(angle)
+            koch(size/3,n-1)
+
+def main():
+    turtle.setup(600,600)
+    turtle.penup()
+    turtle.goto(-200,100)
+    turtle.pendown()
+    turtle.pensize(4)
+    turtle.pencolor('Blue')
+    level=2
+    koch(400,level)
+    turtle.right(120)
+    turtle.pencolor('Red')
+    koch(400,level)
+    turtle.right(120)
+    turtle.pencolor('Green')
+    koch(400,level)
+    turtle.right(120)
+    turtle.hideturtle()
+
+main()
